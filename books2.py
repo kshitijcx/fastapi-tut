988f1da6497ab7b3daaf829eb796a5e0214e7163
kshitijcx <kshitij.cx@gmail.com>
2025-04-08 15:16:20 +0530

    add BookRequest class for validation

--- books2.py
+++ books2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,6 +17,13 @@ class Book:
         self.description=description
         self.rating=rating
 
+class BookRequest(BaseModel):
+    id:int
+    title:str
+    author:str
+    description:str
+    rating:int 
+
 BOOKS = [
     Book(1,'Computer Science Pro', 'kshitij', 'nice book', 5),
     Book(2,'FastAPI', 'kshitij', 'great book', 5),
@@ -31,3 +39,16 @@ async def read_all_books():
 
 #pydantics library used for data modeling, data parsing, efficient error handling
 #pydantics mainly used for data validation of incoming data
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
+
+# The three biggest are:
+
+# 1. .dict() function is now renamed to .model_dump()
+
+# 2. schema_extra function within a Config class is now renamed to json_schema_extra
+
+# 3. Optional variables need a =None example: id: Optional[int] = None
