249e53f017973297579681869421c972cfe00578
kshitijcx <kshitij.cx@gmail.com>
2025-04-08 22:36:37 +0530

    add validation to BookRequest

--- books2.py
+++ books2.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
 class Book:
-    id:int
+    id:int 
     title:str
     author:str
     description:str
@@ -18,11 +19,24 @@ class Book:
         self.rating=rating
 
 class BookRequest(BaseModel):
-    id:int
-    title:str
-    author:str
-    description:str
-    rating:int 
+    # id: Optional[int] = None
+    #describe when id is not needed
+    id: Optional[int] = Field(description="ID is not needed on create", default=None) 
+    title:str = Field(min_length=3)
+    author:str = Field(min_length=1)
+    description:str = Field(min_length=1,max_length=100)
+    rating:int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra":{
+            "example":{
+                "title": "A new book",
+                "author": "kshitij",
+                "description": "A new description of a book",
+                "rating": 5
+            }
+        }
+    }
 
 BOOKS = [
     Book(1,'Computer Science Pro', 'kshitij', 'nice book', 5),
@@ -43,12 +57,17 @@ async def read_all_books():
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    BOOKS.append(find_id(new_book))
 
 # The three biggest are:
-
 # 1. .dict() function is now renamed to .model_dump()
-
 # 2. schema_extra function within a Config class is now renamed to json_schema_extra
-
 # 3. Optional variables need a =None example: id: Optional[int] = None
+
+def find_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    # if len(BOOKS) > 0:
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1;
+    return book
