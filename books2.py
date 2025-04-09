e07e29fd6b500b57f0262a5bbac3070f3c60b77f
kshitijcx <kshitij.cx@gmail.com>
2025-04-09 22:37:38 +0530

    add published_date field and path validation to read_book, delete_book fxns

--- books2.py
+++ books2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path   
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -10,13 +10,15 @@ class Book:
     author:str
     description:str
     rating:int
+    published_date:int
 
-    def __init__(self,id,title,author,description,rating):
+    def __init__(self,id,title,author,description,rating,published_date):
         self.id=id
         self.title=title
         self.author=author
         self.description=description
         self.rating=rating
+        self.published_date=published_date  
 
 class BookRequest(BaseModel):
     # id: Optional[int] = None
@@ -26,6 +28,7 @@ class BookRequest(BaseModel):
     author:str = Field(min_length=1)
     description:str = Field(min_length=1,max_length=100)
     rating:int = Field(gt=0, lt=6)
+    publised_data:int = Field(gt=1999, lt=2031)
 
     model_config = {
         "json_schema_extra":{
@@ -33,18 +36,19 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "kshitij",
                 "description": "A new description of a book",
-                "rating": 5
+                "rating": 5,
+                "published_date": 2029
             }
         }
     }
 
 BOOKS = [
-    Book(1,'Computer Science Pro', 'kshitij', 'nice book', 5),
-    Book(2,'FastAPI', 'kshitij', 'great book', 5),
-    Book(3,'Master Endpoints', 'kshitij', 'awesome book', 5),
-    Book(4,'HP1', 'author 1', 'description', 2),
-    Book(5,'HP2', 'author 2', 'description', 3),
-    Book(6,'HP3', 'author 3', 'description', 1),
+    Book(1,'Computer Science Pro', 'kshitij', 'nice book', 5, 2030),
+    Book(2,'FastAPI', 'kshitij', 'great book', 5, 2030),
+    Book(3,'Master Endpoints', 'kshitij', 'awesome book', 5, 2029),
+    Book(4,'HP1', 'author 1', 'description', 2, 2028),
+    Book(5,'HP2', 'author 2', 'description', 3, 2027),
+    Book(6,'HP3', 'author 3', 'description', 1, 2026),
 ]
 
 @app.get("/books")
@@ -52,7 +56,7 @@ async def read_all_books():
     return BOOKS
 
 @app.get("/books/{book_id}")
-async def read_book(book_id:int):
+async def read_book(book_id:int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
@@ -67,6 +71,14 @@ async def read_book_by_rating(book_rating:int):
 
 #pydantics library used for data modeling, data parsing, efficient error handling
 #pydantics mainly used for data validation of incoming data
+
+@app.get('/books/publish/')
+async def read_books_by_publish_date(published_date:int):
+    booksToReturn = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            booksToReturn.append(book)
+    return booksToReturn
 
 @app.post("/create-book")
 async def create_book(book_request: BookRequest):
@@ -93,7 +105,7 @@ async def update_book(book: BookRequest):
             BOOKS[i]=book
 
 @app.delete('/books/{book_id}')
-async def delete_book(book_id:int):
+async def delete_book(book_id:int = Path(gt=0)):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
